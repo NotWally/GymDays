f2bd1c61e02e95673b97ebacb8e10561a4411b43
Alex <94394627+NotWally@users.noreply.github.com>
2022-06-18 14:01:31 +0100

    Add files via upload

---
+++ timespent.py
@@ -0,0 +1,24 @@
+import time
+
+f = open("totaltime.txt", "r")
+cur_total_time = f.read()
+f.close
+
+start_time = time.time()
+print('counting time. Enjoy your gym session')
+
+input('Press any key to stop: ')
+stop_time = time.time()
+
+time_in_s = int(stop_time - start_time)
+time_in_m = int(time_in_s / 60)
+time_in_h = int(time_in_m / 60)
+
+print("Total Time Spent: ", time_in_h, " Hours ", time_in_m, " Minutes ", time_in_s, " Seconds ")
+new_time = int(cur_total_time) + int(time_in_s)
+new_time = str(new_time)
+
+f = open("totaltime.txt", "w")
+f.write(new_time)
+print("works")
+f.close
